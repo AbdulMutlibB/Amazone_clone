444fbfbc3de82100c611e468bc903e61116c8f2e
AbdulMutlibB <mutlibbutt209@gmail.com>
2021-08-11 15:32:40 +0500

    Amazone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkOut">
+            <h1>ILLUMINATI</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login </h1>
+          </Route>
+          <Route path="/">
+            <Header/>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+            <h1>Home Page_|_</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+// https://www.daraz.pk/shop/aa-brothers?path=index.htm&langFlag=en&lang=en&spm=0.0.categoryBar_4607565.0
---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+function Header() {
+  return (
+    <nav className="header">
+      <Link to="checkOut">
+        <img
+          className="navbar__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionlineone">Abdul Mutlib</span>
+            <span className="header__optionlinetwo">Sign in</span>
+          </div>
+        </Link>
+        <Link to="" className="header__link">
+          <div className="header__option">
+            <span className="header__optionlineone">Return</span>
+            <span className="header__optionlinetwo">Order</span>
+          </div>
+        </Link>
+        <Link className="header__link">
+          <div className="header__option">
+            <span className="header__optionlineone">Yours</span>
+            <span className="header__optionlinetwo">Prime</span>
+          </div>
+        </Link>
+        <Link to="/checkOut" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon/>
+            <span className="header__optionlinetwo header__basketCount" >0</span>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
